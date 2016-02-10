cc537b917bdaf4196db91e9b3c23aa32c8f4cb6d
Dan Caragea <dancaragea@gmail.com>
2016-02-10 02:56:19 +0200

    docs: the demo is oh so pretty :)

--- demo/App.jsx
+++ demo/App.jsx
@@ -12,27 +12,22 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<PopoverWrapper>
-				<header>
-					<h1>React Popover</h1>
-					<a href="https://github.com/terebentina/react-popover">https://github.com/terebentina/react-popover</a>
-				</header>
-				<p>Click the various triggers below and observe the different popover positions, different type of triggers as well as simple or complex popover contents.</p>
-				<p>Also note how, whenever you click away from a popover, it closes as you would expect from a true popover. I hate when you need to re-click the trigger to close the popover - possibly leaving you with several open popovers on a page.</p>
-				<p>But, if you truly want that, you can do that too. See the docs on github</p>
-
-				<div className="box">
-					<p>Your "trigger" can be anything you want: a simple text or html: <Popover trigger="open above" position="top">lorem ipsum...</Popover></p>
-					<div>You can use complex html for the popover content:
-						<Popover className="menu" trigger={<span></span>} position="bottom">
-							<a href="#" onClick={this.menuClick}>Profile</a>
-							<a href="#" onClick={this.menuClick}>Settings</a>
-							<a href="#" onClick={this.menuClick}>Log Out</a>
-						</Popover>
-					</div>
-
-					<p>The popover component comes with minimal styling by default but you can style it however you want: <Popover trigger={<span>web 2.0 nostalgia</span>} position="right" className="web20">popover content</Popover></p>
+			<PopoverWrapper className="box">
+				<p>Your "trigger" can be anything you want: a simple text or html: <Popover trigger="open above" position="top">lorem ipsum...</Popover></p>
+				<div>You can use complex html for the popover content:
+					<Popover className="menu" trigger={<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="24" height="28" viewBox="0 0 24 28"><path d="M24 21v2q0 0.406-0.297 0.703t-0.703 0.297h-22q-0.406 0-0.703-0.297t-0.297-0.703v-2q0-0.406 0.297-0.703t0.703-0.297h22q0.406 0 0.703 0.297t0.297 0.703zM24 13v2q0 0.406-0.297 0.703t-0.703 0.297h-22q-0.406 0-0.703-0.297t-0.297-0.703v-2q0-0.406 0.297-0.703t0.703-0.297h22q0.406 0 0.703 0.297t0.297 0.703zM24 5v2q0 0.406-0.297 0.703t-0.703 0.297h-22q-0.406 0-0.703-0.297t-0.297-0.703v-2q0-0.406 0.297-0.703t0.703-0.297h22q0.406 0 0.703 0.297t0.297 0.703z"></path></svg>} position="bottom">
+						<a href="#" onClick={this.menuClick}>Profile</a>
+						<a href="#" onClick={this.menuClick}>Settings</a>
+						<a href="#" onClick={this.menuClick}>Log Out</a>
+					</Popover>
 				</div>
+
+				<p>The popover component comes with minimal styling by default but you can style it however you want:
+					<Popover trigger={<span>web 2.0 nostalgia</span>} position="right" className="web20">
+						I am oh so pretty :)
+						<span className="beta">beta</span>
+					</Popover>
+				</p>
 			</PopoverWrapper>
 		);
 	}
